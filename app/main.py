adacd647704602738ed3e002186abd10af149e3e
Carlos <73247260+carlosm3@users.noreply.github.com>
2022-11-05 17:39:58 -0600

    Add route

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
-@app.route('/about')
-def about():
-    return '<h1>About Page</h1>'
+
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    params = {"address": request.args.get('address'), "color": request.args.get('color'), "bright": request.args.get('bright'), "cold": request.args.get('cold'), "warmth": request.args.get('warmth')}
+    return render_template('settings.html', title='Settings')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
