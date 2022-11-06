2ef1009381448b635ceabbe3234b4468861322b9
Carlos <73247260+carlosm3@users.noreply.github.com>
2022-11-06 01:33:30 -0600

    Add area selection

--- app/static/js/selection.js
+++ app/static/js/selection.js
@@ -29,45 +29,103 @@ function closeFullscreen() {
     }
 }
 
-var div = document.getElementById('div'), x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+var div = document.getElementById("div"),
+    x1 = 0,
+    y1 = 0,
+    x2 = 0,
+    y2 = 0;
 
 var coordinateX1 = document.getElementById("x1");
 var coordinateY1 = document.getElementById("y1");
 var coordinateX2 = document.getElementById("x2");
 var coordinateY2 = document.getElementById("y2");
 
-function reCalc() { //This will restyle the div
-    var x3 = Math.min(x1,x2); //Smaller X
-    var x4 = Math.max(x1,x2); //Larger X
-    var y3 = Math.min(y1,y2); //Smaller Y
-    var y4 = Math.max(y1,y2); //Larger Y
-    div.style.left = x3 + 'px';
-    div.style.top = y3 + 'px';
-    div.style.width = x4 - x3 + 'px';
-    div.style.height = y4 - y3 + 'px';
+function reCalc() {
+    //This will restyle the div
+    var x3 = Math.min(x1, x2); //Smaller X
+    var x4 = Math.max(x1, x2); //Larger X
+    var y3 = Math.min(y1, y2); //Smaller Y
+    var y4 = Math.max(y1, y2); //Larger Y
+    div.style.left = x3 + "px";
+    div.style.top = y3 + "px";
+    div.style.width = x4 - x3 + "px";
+    div.style.height = y4 - y3 + "px";
 }
 
-onmousedown = function(e) {
+// onmousedown = function (e) {
+//     div.hidden = 0; //Unhide the div
+//     x1 = e.clientX; //Set the initial X
+//     y1 = e.clientY; //Set the initial Y
+//     reCalc();
+// };
+
+// onmousemove = function (e) {
+//     x2 = e.clientX; //Update the current position X
+//     y2 = e.clientY; //Update the current position Y
+//     // console.log(x2, y2);
+//     reCalc();
+// };
+
+// onmouseup = function (e) {
+//     div.hidden = 1; //Hide the div
+//     coordinateX1.value = x1;
+//     coordinateY1.value = y1;
+//     coordinateX2.value = x2;
+//     coordinateY2.value = y2;
+// };
+
+// elem.addEventListener("mousedown", function (e) {
+//     div.hidden = 0; //Unhide the div
+//     x1 = e.clientX; //Set the initial X
+//     y1 = e.clientY; //Set the initial Y
+//     reCalc();
+// });
+
+function mouseDown(e) {
     div.hidden = 0; //Unhide the div
     x1 = e.clientX; //Set the initial X
     y1 = e.clientY; //Set the initial Y
     reCalc();
-};
+}
 
-onmousemove = function(e) {
-    x2 = e.clientX; //Update the current position X
-    y2 = e.clientY; //Update the current position Y
-    reCalc();
-};
+// elem.addEventListener("mouseup", function (e) {
+//     div.hidden = 1; //Hide the div
+//     coordinateX1.value = x1;
+//     coordinateY1.value = y1;
+//     coordinateX2.value = x2;
+//     coordinateY2.value = y2;
+//     overlay.style.display = "none";
+//     closeFullscreen();
+// });
 
-onmouseup = function(e) {
+function mouseUp (e) {
     div.hidden = 1; //Hide the div
     coordinateX1.value = x1;
     coordinateY1.value = y1;
     coordinateX2.value = x2;
     coordinateY2.value = y2;
-};
+    overlay.style.display = "none";
+    closeFullscreen();
+    elem.removeEventListener("mousemove");
+}
 
-toggleSelection.addEventListener("click", function() {
+// elem.addEventListener("mousemove", function (e) {
+//     x2 = e.clientX; //Update the current position X
+//     y2 = e.clientY; //Update the current position Y
+//     reCalc();
+// });
+
+function mouseMove(e) {
+    x2 = e.clientX; //Update the current position X
+    y2 = e.clientY; //Update the current position Y
+    reCalc();
+}
+
+toggleSelection.addEventListener("click", function () {
+    elem.addEventListener("mousedown", mouseDown, { once: true });
+    elem.addEventListener("mouseup", mouseUp, { once: true });
+    elem.addEventListener("mousemove", mouseMove);
+    openFullscreen();
+    overlay.style.display = "block";
     console.log("clicked");
 });
