b0265f7be2cb9aaf72c64bad4005b8f05ae46ea7
Carlos <73247260+carlosm3@users.noreply.github.com>
2022-11-05 18:49:33 -0600

    Update framework

--- app/main.py
+++ app/main.py
@@ -9,10 +9,15 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/settings', methods=['GET', 'POST'])
-def settings():
+@app.route('/playground', methods=['GET', 'POST'])
+def playground():
     params = {"address": request.args.get('address'), "color": request.args.get('color'), "bright": request.args.get('bright'), "cold": request.args.get('cold'), "warmth": request.args.get('warmth')}
-    return render_template('settings.html', title='Settings')
+    return render_template('playground.html', title='Playground')
+
+
+@app.route('/streaming', methods=['GET', 'POST'])
+def streaming():
+    return render_template('streaming.html', title='Streaming')
 
 
 if __name__ == '__main__':
