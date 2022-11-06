24ac112bfa7b8bb0848a9d7f50857dbb8f11fd44
Shepherd <shettyaaryan2003@gmail.com>
2022-11-05 20:49:49 -0600

    Create avgRGB v2 (screencap).py

---
+++ avgRGB v2 (screencap).py
@@ -0,0 +1,61 @@
+import cv2, time, imutils, pyautogui
+import keyboard as k
+import numpy as np
+from threading import Thread
+from bulbInterface import lightbulb as lb
+
+lt1 = lb("192.168.137.105",(0, 0, 0), 0)
+
+def calculateAvg(ar,ag,ab,count):
+    r = round(ar/count)
+    g = round(ag/count)
+    b = round(ab/count)
+    return (r,g,b)
+
+def RGBt(rgb):
+    lt1.RGBScale(rgb)
+
+def main():
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    start = time.time()
+    ab = ag = ar = count = 0
+    lum = 0
+
+    while True:
+        cv2.imshow("Unironic Image Name", image)
+        b,g,r = image.mean(axis=(0,1))
+        end = time.time()
+        if end < start + 1:
+            ab += b
+            ag += g
+            ar += r
+            count += 1
+        else:
+            rgb = calculateAvg(ar,ag,ab,count)
+            ab = ag = ar = count = 0
+            sendRGB = Thread(target = RGBt(rgb))
+            sendRGB.start()
+            start = time.time()
+        time.sleep(1/40)
+
+        if k.is_pressed('b'):
+            lum += 10
+            if lum >= 255:
+                lum = 255
+            lt1.changeBright(lum)
+        elif k.is_pressed('v'):
+            lum -= 10
+            if lum <= 0:
+                lum = 0
+            lt1.changeBright(lum)
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        image = pyautogui.screenshot()
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    cv2.destroyAllWindows()
+
+t1 = Thread(target = main())
+t1.start()
