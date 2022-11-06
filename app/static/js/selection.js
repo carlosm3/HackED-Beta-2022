3b2c9f80c0cf84774caedf12fde8fc8b85c306fd
Carlos <73247260+carlosm3@users.noreply.github.com>
2022-11-05 23:47:38 -0600

    Update rectangle selection

--- app/static/js/selection.js
+++ app/static/js/selection.js
@@ -1,40 +1,3 @@
-// var container = document.getElementById("container");
-// var selection = document.createElement("div");
-// selection.classList.add("selection-box");
-
-// container.addEventListener("mousedown", function (e) {
-//     var click_y = e.pageY;
-//     var click_x = e.pageX;
-
-//     selection.style.top = click_y + "px";
-//     selection.style.left = click_x + "px";
-//     selection.style.width = 0;
-//     selection.style.height = 0;
-//     selection.appendChild(container);
-
-//     container.addEventListener("mousemove", function (e) {
-//         var move_x = e.pageX,
-//             move_y = e.pageY,
-//             width = Math.abs(move_x - click_x),
-//             height = Math.abs(move_y - click_y),
-//             new_x,
-//             new_y;
-
-//         new_x = move_x < click_x ? click_x - width : click_x;
-//         new_y = move_y < click_y ? click_y - height : click_y;
-
-//         selection.style.width = width + "px";
-//         selection.style.height = height + "px";
-//         selection.style.top = new_y + "px";
-//         selection.style.left = new_x + "px";
-//     });
-
-//     container.addEventListener("mouseup", function (e) {
-//         container.removeEventListener("mousemove");
-//         selection.remove();
-//     });
-// });
-
 const overlay = document.getElementById("overlay");
 const area = document.getElementById("selection-area");
 
@@ -68,89 +31,32 @@ function closeFullscreen() {
     }
 }
 
-function actionOne(e) {
-    area.style.display = "block";
-    startPoint = [e.clientX, e.clientY];
-    document.addEventListener("mousemove", drag);
-    document.addEventListener("mouseup", actionTwo);
+var div = document.getElementById('div'), x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+function reCalc() { //This will restyle the div
+    var x3 = Math.min(x1,x2); //Smaller X
+    var x4 = Math.max(x1,x2); //Larger X
+    var y3 = Math.min(y1,y2); //Smaller Y
+    var y4 = Math.max(y1,y2); //Larger Y
+    div.style.left = x3 + 'px';
+    div.style.top = y3 + 'px';
+    div.style.width = x4 - x3 + 'px';
+    div.style.height = y4 - y3 + 'px';
 }
 
-function actionTwo(e) {
-    document.removeEventListener("mousedown");
-    document.removeEventListener("mousemove");
-    document.removeEventListener("mouseup");
-    endPoint = [e.clientX, e.clientY];
-}
+onmousedown = function(e) {
+    div.hidden = 0; //Unhide the div
+    x1 = e.clientX; //Set the initial X
+    y1 = e.clientY; //Set the initial Y
+    reCalc();
+};
 
-function drag(e) {
-    endPoint = [e.clientX, e.clientY];
-    area.style.left = startPoint[0] + "px";
-    area.style.top = startPoint[1] + "px";
-    area.style.width = endPoint[0] - startPoint[0] + "px";
-    area.style.height = endPoint[1] - startPoint[1] + "px";
-}
+onmousemove = function(e) {
+    x2 = e.clientX; //Update the current position X
+    y2 = e.clientY; //Update the current position Y
+    reCalc();
+};
 
-document.addEventListener("mousedown", function (e) {
-    // startPoint = [event.clientX, event.clientY];
-    // area.style.left = startPoint[0] + "px";
-    // area.style.top = startPoint[1] + "px";
-    // area.style.width = "0px";
-    // area.style.height = "0px";
-    // area.style.display = "block";
-    // overlay.style.display = "block";
-    // startPoint = [event.clientX, event.clientY];
-    // overlay.style.display = "block";
-    // area.style.display = "block";
-    // area.style.left = startPoint[0] + "px";
-    // area.style.top = startPoint[1] + "px";
-    // console.log("Mouse down: " + startPoint);
-});
-
-document.addEventListener("mouseup", function (e) {
-    // if (document.fullscreenElement) {
-    //     closeFullscreen();
-    // }
-    // overlay.style.display = "none";
-    // endPoint = [event.clientX, event.clientY];
-    // area.style.display = "none";
-    // console.log("Mouse up: " + endPoint);
-    // x1.value = startPoint[0];
-    // y1.value = startPoint[1];
-    // x2.value = endPoint[0];
-    // y2.value = endPoint[1];
-});
-
-document.addEventListener("mousemove", function (e) {
-    // area.style.width = e.clientX - startPoint[0] + "px";
-    // area.style.height = e.clientY - startPoint[1] + "px";
-    // console.log("Mouse move: " + [e.clientX, e.clientY]);
-});
-
-document.getElementById("area").addEventListener("click", function () {
-    // openFullscreen();
-    overlay.style.display = "block";
-    document.addEventListener("mousedown", actionOne);
-    console.log("clicked");
-});
-
-// var mousedownID = -1;  //Global ID of mouse down interval
-// function mousedown(event) {
-//     if (mousedownID == -1)  //Prevent multimple loops!
-//         mousedownID = setInterval(whilemousedown, 100 /*execute every 100ms*/);
-
-// }
-// function mouseup(event) {
-//     if (mousedownID != -1) {  //Only stop if exists
-//         clearInterval(mousedownID);
-//         mousedownID = -1;
-//     }
-
-// }
-// function whilemousedown() {
-//     /*here put your code*/
-// }
-// //Assign events
-// document.addEventListener("mousedown", mousedown);
-// document.addEventListener("mouseup", mouseup);
-// //Also clear the interval when user leaves the window with mouse
-// document.addEventListener("mouseout", mouseup);
+onmouseup = function(e) {
+    div.hidden = 1; //Hide the div
+};
